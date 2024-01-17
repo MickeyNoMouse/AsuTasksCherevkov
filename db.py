c11f965d597083e7b146d58dc79cd41cc1485ec7
Mikhail_Cherevkov <mikhailcherevkov@gmail.com>
2024-01-17 18:59:07 +0700

    Initial commit

---
+++ db.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, text, insert, select
+from sqlalchemy.ext.asyncio import create_async_engine
+import asyncio
+from config import settings
+from models.model import Base, Employee
+
+
+
+ur_s = settings.POSTGRES_DATABASE_URLS
+#ur_a = settings.POSTGRES_DATABASE_URLA
+
+print(ur_s)
+#engine = create_async_engine(ur_p, echo=True)
+engine_s = create_engine(ur_s, echo=True)
+
+#async def f():
+  #  async with engine.begin() as conn:
+       # answer = await conn.execute(text("select version()"))
+      #  print(f"answer = {answer.all()}")
+
+#asyncio.run(f())
+
+#def create_tables():
+    #Base.metadata.drop_all(bind=engine_s)
+    #Base.metadata.create_all(bind=engine_s)
+
+#def f():
+   #with engine_s.connect() as conn:
+        #answer = conn.execute(text('select * from public."Employee";'))
+       # print(f"answer = {answer.all()}")
---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from public.employees import employees_router #, init_db
+from datetime import datetime
+#from db import create_tables
+
+app = FastAPI()
+
+#create_tables()
+
+app.include_router(employees_router)
+
+#@app.on_event("startup")
+#def on_startup():
+    #open("log.txt", mode="a").write(f'{datetime.utcnow()}: Begin\n')
+    #init_db()
+
+#@app.on_event("shutdown")
+#def shutdown():
+    #open("log.txt", mode="a").write(f'{datetime.utcnow()}: End\n')
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
