3344e9e99af27e2f2883da24b256b76e2e9defff
Mikhail_Cherevkov <mikhailcherevkov@gmail.com>
2024-01-21 17:27:42 +0700

    add migrations2

--- migrations/env.py
+++ migrations/env.py
@@ -3,11 +3,21 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from config import settings
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+
+config.set_section_option(section, 'POSTGRES_USER', settings.POSTGRES_USER)
+config.set_section_option(section, 'POSTGRES_PORT', settings.POSTGRES_PORT)
+config.set_section_option(section, 'POSTGRES_PASSWORD', settings.POSTGRES_PASSWORD)
+config.set_section_option(section, 'POSTGRES_DB', settings.POSTGRES_DB)
+config.set_section_option(section, 'POSTGRES_HOST', settings.POSTGRES_HOST)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
